c39b67a1b152faccaf6804f04663f92661fc1faf
Lisa Martens <lemnis.martens@gmail.com>
2022-07-18 22:26:03 +0200

    Fix file

--- voiceover.mjs
+++ voiceover.mjs
@@ -1,8 +1,11 @@
-import { VoiceOver } from '@accesslint/voiceover';
+import { VoiceOver } from "@accesslint/voiceover";
 
 const voiceOver = new VoiceOver();
-await voiceOver.launch(); // start VoiceOver screen reader
-await new Promise(r => setTimeout(r, 4000));
-console.log(await voiceOver.lastPhrase()); // print last phrase on navigation
-// perform actions using seek, rotor, and execute
-await voiceOver.quit(); // stop VoiceOver
+
+(async () => {
+  await voiceOver.launch(); // start VoiceOver screen reader
+  await new Promise((r) => setTimeout(r, 4000));
+  console.log(await voiceOver.lastPhrase()); // print last phrase on navigation
+  // perform actions using seek, rotor, and execute
+  await voiceOver.quit(); // stop VoiceOver
+})();
