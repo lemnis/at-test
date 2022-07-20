15df9eb0472ef1354155a917cbb1551ce625bf29
Lisa Martens <lemnis.martens@gmail.com>
2022-07-20 02:54:25 +0200

    Add progress

--- tests/html/progress.ts
+++ tests/html/progress.ts
@@ -1,28 +1,32 @@
 /// <reference path="../../codeceptjs/steps.d.ts" />
-
-import { equal, ok } from "assert";
+import { expect } from "chai";
+import snapshot from "snap-shot-it";
 
 Feature("Progress").tag("html/progress");
 
 const html = () => /*html*/ `<progress id="test"></progress>`;
-const htmlWithLabel = () => /*html*/ `<progress id="test"></progress><label for="test">Label</label>`;
+const htmlWithLabel = () =>
+  /*html*/ `<progress id="test"></progress><label for="test">Label</label>`;
 
 Scenario("Should be targetable when having a label", async ({ I }) => {
   I.setContent(htmlWithLabel());
-  ok(await I.grabAXNode("#test"));
+  snapshot(await I.grabATOutput("#test") as any);
+  expect(await I.grabATOutput("#test")).to.not.be.undefined;
+
   I.setContent(html());
-  equal(await I.grabAXNode("#test"), undefined);
+  snapshot(await I.grabATOutput("#test") as any);
+  expect(await I.grabATOutput("#test")).to.be.undefined;
 }).tag("targetable");
 
 Scenario("Should have role", async ({ I }) => {
   I.setContent(htmlWithLabel());
-  equal((await I.grabAXNode("#test"))?.role, "progressbar");
+  expect(await I.grabATOutput("#test")).to.have.role("progressbar");
 }).tag("role");
 
 Scenario("Should have accessible name", async ({ I }) => {
   I.setContent(htmlWithLabel());
-  equal((await I.grabAXNode("#test"))?.name, "Label");
+  expect(await I.grabATOutput("#test")).to.have.name("Label");
 }).tag("name");
 
-Scenario.todo('Shoud convey current value').tag("value");
-Scenario.todo('Shoud convey indeterminate state').tag("indeterminate");
+Scenario.todo("Shoud convey current value").tag("value");
+Scenario.todo("Shoud convey indeterminate state").tag("indeterminate");
