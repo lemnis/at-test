6a5960fa35675c90da1b01899ca38f6a8aadafb8
Lisa Martens <lemnis.martens@gmail.com>
2022-07-25 14:51:29 +0200

    Fix skipping color test in webdriver

--- tests/html/input/color.ts
+++ tests/html/input/color.ts
@@ -1,14 +1,14 @@
 /// <reference path="../../../codeceptjs/steps.d.ts" />
-
-import { equal, ok } from "assert";
+import { ok } from "assert";
 import { expect } from "chai";
 import snapshot from "snap-shot-it";
+import { BROWSERS, getBrowserName } from "../../utils/setup";
 
 const helpers = codeceptjs.config.get("helpers");
-const currentEngine: "webkit" | "firefox" | "chromium" =
-  (helpers.Playwright || helpers.WebDriver).browser;
+const isFirefox = getBrowserName() === BROWSERS.FIREFOX;
 
-Feature("Color").tag("html/input/color");
+// Firefox crashes, so just skip the test for now.
+(isFirefox ? xFeature : Feature)("Color").tag("html/input/color");
 
 const html = /*html*/ `
   <button id="start" aria-label="start">start</button>
@@ -18,8 +18,6 @@ const html = /*html*/ `
 Scenario(
   "Should be targetable when having a label",
   async function (this: any, { I }) {
-    if (currentEngine === "firefox") this.skip();
-
     I.setContent(html);
 
     if (helpers.ChromevoxHelper || helpers.VoiceOverHelper) {
@@ -38,8 +36,6 @@ Scenario(
 ).tag("targetable");
 
 Scenario("Should have role", async function (this: any, { I }) {
-  if (currentEngine === "firefox") this.skip();
-
   I.setContent(html);
 
   if (helpers.ChromevoxHelper || helpers.VoiceOverHelper) {
@@ -55,8 +51,6 @@ Scenario("Should have role", async function (this: any, { I }) {
 }).tag("role");
 
 Scenario("Should have accessible name", async function (this: any, { I }) {
-  if (currentEngine === "firefox") this.skip();
-
   I.setContent(html);
 
   if (helpers.ChromevoxHelper || helpers.VoiceOverHelper) {
---
+++ tests/utils/setup.ts
@@ -0,0 +1,34 @@
+export enum BROWSERS {
+  CHROMIUM = 'chromium',
+  SAFARI = 'safari',
+  CHROME = 'chrome',
+  WEBKIT = 'webkit',
+  FIREFOX = 'firefox',
+}
+
+export enum ASSISTIVE_TECHNOLOGY {
+  CHROMEVOX = 'ChromeVox',
+  VOICEOVER = 'VoiceOver',
+  NONE = 'None',
+}
+
+export const helpers: {
+  Playwright?: CodeceptJS.Playwright & Record<string, any>,
+  WebDriver?: CodeceptJS.WebDriver & Record<string, any>,
+  VoiceOverHelper?: Record<string, any>,
+  ChromeVoxHelper?: Record<string, any>,
+} = codeceptjs.config.get("helpers");
+
+export const getDriver = () => {
+  return helpers.Playwright ? 'Playwright' : 'WebDriver';
+}
+
+export const getBrowserName = (): BROWSERS => {
+  return helpers.Playwright?.browser || helpers.WebDriver?.browser;
+}
+
+export const getAT = () => {
+  if(helpers.VoiceOverHelper) return ASSISTIVE_TECHNOLOGY.VOICEOVER;
+  if(helpers.ChromeVoxHelper) return ASSISTIVE_TECHNOLOGY.CHROMEVOX
+  return ASSISTIVE_TECHNOLOGY.NONE;
+}
